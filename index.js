c1ae6504d3c6096c58339c1f728a1a87b692c232
mateusz <mateusz@blazar.localdomain>
2022-09-05 16:29:16 +0200

    Api with raw sql queries

--- index.js
+++ index.js
@@ -12,17 +12,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const notes = await sequelize.query('SELECT * FROM notes', {
-      type: QueryTypes.SELECT,
-    })
-    console.log(notes)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database: ', error)
-  }
-}
+app.get('/api/notes', async (req, res) => {
+  const notes = await sequelize.query('SELECT * FROM notes', {
+    type: QueryTypes.SELECT,
+  })
+  res.json(notes)
+})
 
-main()
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
